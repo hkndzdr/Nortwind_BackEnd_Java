56ceaf46e601e19b5fa143f0e83ba324c1c039df
Hakan Dizdar <hakandizdar@gmail.com>
2021-05-28 00:23:23 +0300

    JPA repository integration

--- src/main/java/kodlamaio/northwind/entities/concretes/Product.java
+++ src/main/java/kodlamaio/northwind/entities/concretes/Product.java
@@ -3,6 +3,8 @@ package kodlamaio.northwind.entities.concretes;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Table;
@@ -23,8 +25,8 @@ public class Product {
 	@Column(name="product_id")
 	private int id;
 	
-	@Column(name="category_id")
-	private int categoryId;
+	/*@Column(name="category_id")
+	private int categoryId;*/
 	
 	@Column(name="product_name")
 	private String productName;
@@ -38,6 +40,8 @@ public class Product {
 	@Column(name="quantity_per_unit")
 	private String quantityPerUnit;
 	
-
+	@ManyToOne()
+	@JoinColumn(name="category_id")
+	private Category category;
 	
 }
